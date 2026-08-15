d1ccdb725945a8542f4ac553fdd7f50737ae0d8a
Bassem Sherief <108496148+BassemAdam@users.noreply.github.com>
2026-08-15 21:26:26 +0300

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        # Practical Usability : identify duplication in
+        #--------------------------------------
+
+        # solution 1
+        # Time O(n) avg - note that dictionery lookup can degrade in the worst case to become O(n) instead of O(1) so in worst case Time could be O(n^2) rarely
+        # Space O(n)
+        # d = {}
+        # for num in nums:
+        #     d[num] = d[num] + 1 if num in d else 1 
+        #     if d[num] == 2:
+        #         return True
+        # return False 
+
+        #-------------------------------------
+
+        # solution 2
+        # Time O(nlogn + n = nlogn) python Timsort  
+        # space O(0)
+        # nums.sort()
+        # for i in range(len(nums) - 1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
+        # return False
+
+        # solution 3
+        return len(nums) != len(set(nums))
