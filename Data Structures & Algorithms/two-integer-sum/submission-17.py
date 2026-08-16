ad42e73d28beb7eb4a835421f9720d0a4b7e6a8b
Bassem Sherief <108496148+BassemAdam@users.noreply.github.com>
2026-08-16 12:07:53 +0300

    Add: two-integer-sum - submission-17

---
+++ Data Structures & Algorithms/two-integer-sum/submission-17.py
@@ -0,0 +1,34 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # solution 1 - Hashmap
+        # d = {} # Space O(n)
+        # for i in range(len(nums)): # Time O(n)
+        #     d[nums[i]] = i # Time O(1) worst case O(n) propagation
+        # for i in range(len(nums)): # Time O(n)
+        #     ptrJValue = target - nums[i] # Time O(1)
+        #     if ptrJValue in d and i != d[ptrJValue]: # Time O(1)
+        #         return [i,d[ptrJValue]] # Time O(1)
+        # Complexity : Time O(n) Space O(n)
+        #----------------------------------------------------------------
+
+        # solution 2 - Brute Force
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         if nums[i] + nums[j] == target and i != j:
+        #             return [i,j]
+        # Complexity : Time O(n^2) Space O(2)
+        #----------------------------------------------------------------
+        
+        # solution 1 - better rewritten 
+        d = {}
+        for i,num in enumerate(nums):
+            complement = target - num
+            if complement in d:
+                return sorted([i,d[complement]])
+            d[num] = i
+
+        # Practical Usability
+        # 1- the idea is that we can lookup with complement instead of searching brute force to find pairs
+        # so if in inventory or ecommerce and you want to find another item for user that fit its total vouchers for example you can do that complement hash search 
