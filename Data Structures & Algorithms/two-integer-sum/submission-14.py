3d20e1085564ff0d5cfa6e5b2f6dcb43019e21c5
Bassem Sherief <108496148+BassemAdam@users.noreply.github.com>
2026-08-16 11:58:23 +0300

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # solution 1 - Hashmap
+        # d = {} # Space O(n)
+        # for i in range(len(nums)): # Time O(n)
+        #     d[nums[i]] = i # Time O(1) worst case O(n) propagation
+        # for i in range(len(nums)): # Time O(n)
+        #     ptrJValue = target - nums[i] # Time O(1)
+        #     if ptrJValue in d and i != d[ptrJValue]: # Time O(1)
+        #         return [i,d[ptrJValue]] # Time O(1)
+        # Complexity : Time O(n) Space O(n)
+
+        # solution 2 - Brute Force
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i,j]
