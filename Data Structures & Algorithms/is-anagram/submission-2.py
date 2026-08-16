50de2f14b109a73cd988979c5d93948d3505e2e4
Bassem Sherief <108496148+BassemAdam@users.noreply.github.com>
2026-08-16 10:36:29 +0300

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # solution 1
+        if len(s) != len(t): return False
+        s = list(s)
+        t = list(t)
+        s.sort()
+        t.sort()
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return False
+        return True
