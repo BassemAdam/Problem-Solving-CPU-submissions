e6157bc47e1aa56eb633c5c07b842881e95d56cc
Bassem Sherief <108496148+BassemAdam@users.noreply.github.com>
2026-09-19 17:54:46 +0300

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canJump(self, nums: list[int]) -> bool:
+        
+        def backtrack(i,cache):
+            if i in cache: return cache[i]
+            if i >= len(nums) - 1:
+                cache[i] = True 
+                return True
+
+            steps = nums[i]
+            
+            if steps == 0: 
+                cache[i] = False
+                return False
+            for j in range(steps,0,-1):
+                if i + j >= len(nums) - 1:
+                    cache[i] =  True
+                    return True  
+                if backtrack(j+i,cache):
+                    cache[i] =  True
+                    return cache[i] 
+            cache[i] = False
+            return cache[i] 
+
+        return backtrack(0,{})
+        
